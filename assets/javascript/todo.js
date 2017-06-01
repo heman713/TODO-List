5f3e49d731ada02b134b55e964aea04a6a76bb34
Himanshu Yadav <yadav.himanshu713@gmail.com>
2017-06-01 08:02:46 +0530

    ToDo App using jquery
    FIrst Commit

---
+++ assets/javascript/todo.js
@@ -0,0 +1,22 @@
+$("ul").on("click","li",function() {
+    $(this).toggleClass("delete");
+})
+
+$("span").on("click",function(event) {
+    $(this).parent().fadeOut(500,function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("input[type=text]").on("keypress", function(event) {
+    if(event.which === 13){
+        var todo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X </span>" +todo+ "</li>");
+    }
+})
+
+$("#header").click(function(){
+    $("input[type=text]").fadeToggle(500,"swing");
+})
